b8fc284dd7b5c3bf02f0434dd2ff159b48e1701d
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-28 11:27:07 -0800

    getting keys, values, items

--- sec-03/dicts.py
+++ sec-03/dicts.py
@@ -5,3 +5,29 @@ print(my_dict['key1'])
 prices = { 'banana': 3.00, 'apple': 1.99, 'orange': 5.20 }
 
 print(prices['apple'])
+
+# ADDING
+prices['milk'] = 2.99
+print(prices)
+
+# DELETING
+del prices['apple']
+
+print(prices)
+
+#MODIFING
+prices['banana'] = prices['banana'] * 1.25
+
+print(prices)
+
+# KEYS
+keys = prices.keys()
+print(keys)
+
+# VALUES
+values = prices.values()
+print(values)
+
+# ITEMS
+items = prices.items()
+print(items)
