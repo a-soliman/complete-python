afd493d6a3f91dcaeb2c05fafc9cf478feda818d
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-28 12:03:32 -0800

    basics of tuples

---
+++ sec-03/tuples.py
@@ -0,0 +1,15 @@
+t = (1,2,3)
+
+my_list = [1,2,3]
+
+print('TYPE: ', type(t))
+print(type(my_list))
+
+print('LEN: ', len(t))
+print(t[0])
+
+t = ('a', 'a', 'b')
+print('COUNT: ', t.count('a'))
+print('INDEX: ', t.index('a'))
+
+# MUTAPILITY
