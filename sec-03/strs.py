1f74ad211bb19a43cf8980008fa29c4433ad7ac7
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-28 10:22:52 -0800

    over strings

---
+++ sec-03/strs.py
@@ -0,0 +1,14 @@
+my_str = "This is an example"
+sliced_text = my_str[0:5:1]
+print(my_str)
+print(len(sliced_text))
+print(my_str[0])
+print(my_str[-1])
+
+x = 'Hello world'
+print(x.upper())
+print(x.lower())
+print(x.title())
+print(x.capitalize())
+
+print(x.lower().split())
