f3bb14f9a29efe011ac80735ec4970eb698542cd
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-03-01 07:42:33 -0800

    use case with set 'get unique value'

--- sec-03/sets.py
+++ sec-03/sets.py
@@ -4,5 +4,18 @@ my_set = set()
 my_set.add(1)
 my_set.add(2)
 
-
+# Conflicting
+my_set.add(2)
 print(my_set)
+
+# useCase
+
+def get_unique( list ):
+    new_set = set()
+
+    for value in list:
+        new_set.add(value)
+
+    return new_set
+
+print(get_unique([1,2,1,2,5,4,6,4,5]))
