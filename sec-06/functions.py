c9bdacf239eb244dd4d5184c2494e49b79c75917
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-03-02 08:39:30 -0800

    funcitons

---
+++ sec-06/functions.py
@@ -0,0 +1,11 @@
+def name_function():
+      '''
+      This function just says hello
+      '''
+      print('Hello!')
+
+
+def dog_check( string ):
+      return  'dog' in string.lower()
+
+print(dog_check('there is no  in here'))
