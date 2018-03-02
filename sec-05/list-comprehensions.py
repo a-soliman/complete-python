21ca87158a2168c99678eb25083fa9780d80667e
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-03-02 07:37:22 -0800

    more with list-comprehenssions

--- sec-05/list-comprehensions.py
+++ sec-05/list-comprehensions.py
@@ -17,5 +17,21 @@ print(l2)
 l3 = [ num + 2 for num in range(0, 11) if num %2 == 0]
 print(l3)
 
+vowels = 'aioue'
+sent = 'Hey there my name is ahmed'
 
+l4 = [ x.upper() for x in sent if x in vowels ]
+
+print(l4)
+
+c = [32, 0, -10, 50]
+f = [ ( (9/5)*temp + 32) for temp in c]
+print(f)
+
+f2 = []
+
+for temp in c:
+      f2.append( (9/5)* temp + 32)
+
+print(f2)
 
