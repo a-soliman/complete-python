222170cf00901959f6b584d030f3dcdeccb3f3ea
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-03-02 07:36:57 -0800

    more with list-comprehensssion with nested conditionals

--- sec-05/list-comprehensions.py
+++ sec-05/list-comprehensions.py
@@ -10,4 +10,12 @@ new_list = [ x for x in my_string]
 
 print(new_list)
 
+l2 = [ letter for letter in 'Hey, How is it goint']
+
+print(l2)
+
+l3 = [ num + 2 for num in range(0, 11) if num %2 == 0]
+print(l3)
+
+
 
