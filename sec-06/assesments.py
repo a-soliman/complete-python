3a3ba1268737a2cb3022935e2985f0f3dba66fd3
a-soliman <ahmed.soliman@programmer.net>
2018-03-03 09:55:20 -0800

    assessment: Animal crackers

--- sec-06/assesments.py
+++ sec-06/assesments.py
@@ -13,3 +13,15 @@ def lesser_of_two( a,b ):
 
 print(lesser_of_two(2,4))
 print(lesser_of_two(2,5))
+
+print('='*20)
+
+# Animal crackers:
+# write a func takes 2 string and return true if both words begin with same letter
+
+def animal_crackers( string1, string2):
+    return string1[0].lower() == string2[0].lower()
+
+
+print(animal_crackers('Levis', 'Llama'))
+print(animal_crackers('Levis', 'kangaroo'))
