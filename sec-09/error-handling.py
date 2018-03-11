f213b7c5f2f664c85a233ed6edbf9ff117db468b
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-03-11 08:45:01 -0700

    more with testing

--- sec-09/error-handling.py
+++ sec-09/error-handling.py
@@ -1,16 +1,32 @@
-def add ( n1, n2 ):
-    return n1 + n2
+# def add ( n1, n2 ):
+#     return n1 + n2
 
 
-add(10, 20)
+# add(10, 20)
 
-num1 = 10
-num2 = input('Please provide anumber: ')
+# num1 = 10
+# num2 = input('Please provide anumber: ')
 
-try:
-    result = add(num1, num2)
-except:
-    print('There was an error!')
-else:
-    print('add went well!')
-    print(result)
+# try:
+#     result = add(num1, num2)
+# except:
+#     print('There was an error!')
+# else:
+#     print('add went well!')
+#     print(result)
+
+
+def ask_for_int():
+    
+    while True:
+        try: 
+            result = int(input('Please provide a number: ').strip())
+        except:
+            print('woops! that is not a number..')
+        else:
+            print('Thank you!')
+            break
+        finally:
+            print('End of try/except/finally')
+
+ask_for_int()
