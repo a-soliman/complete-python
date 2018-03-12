8b73c1ade88870507fdace28f1af4aa2a71c345e
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-03-12 08:03:11 -0700

    importing and testing packages

--- sec-10/myprogram.py
+++ sec-10/myprogram.py
@@ -1,3 +1,7 @@
-import mymodule
+# import mymodule
+from main_package import some_main_script
+from main_package.sub_package import mysubscript
 
-mymodule.myfunc()
+
+some_main_script.report_main()
+mysubscript.sub_report()
