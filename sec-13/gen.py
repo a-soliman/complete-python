0bfb091211ef3c01b50c0426592c763128f5e6a8
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-03-13 09:00:08 -0700

    more with generator

--- sec-13/gen.py
+++ sec-13/gen.py
@@ -1,7 +1,16 @@
 import random
+
 def gensquares(n):
       for num in range(n):
             yield num**2
 
 for num in gensquares(10):
+      print(num)
+
+print('='*20)
+def rand_num(low, high, n):
+      for x in range(n):
+            yield random.randint(low,high)
+
+for num in rand_num(1, 10, 15):
       print(num)
