d94d09130a287db9dc6774e24b81eedb2d01e59c
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-03-13 07:57:28 -0700

    passing func as a param

--- sec-12/decorators.py
+++ sec-12/decorators.py
@@ -13,20 +13,33 @@
 # print(greet())
 
 # nested func
+
+# def hello(name='Ahmed'):
+#       print('This is hello() has been excuted')
+
+#       def greet(name):
+#             return '\n this is the greet() from hello(), saying hello {}'.format(name)
+      
+#       def welcome():
+#             return '\n this is welcome() inside hello()'
+      
+#       print('I am going to return a function')
+
+#       if name == 'Ahmed':
+#             return greet(name)
+#       else:
+#             return welcome()
+
+# print(hello('zzz'))
+
+
+# passing a func as param
+
 def hello(name='Ahmed'):
-      print('This is hello() has been excuted')
+      return 'Hey {}'.format(name)
 
-      def greet(name):
-            return '\n this is the greet() from hello(), saying hello {}'.format(name)
-      
-      def welcome():
-            return '\n this is welcome() inside hello()'
-      
-      print('I am going to return a function')
+def other_func(func):
+      print('This is the other_func()')
+      print(func())
 
-      if name == 'Ahmed':
-            return greet(name)
-      else:
-            return welcome()
-
-print(hello('zzz'))
+other_func(hello)
