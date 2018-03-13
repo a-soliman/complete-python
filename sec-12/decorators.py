deedb0313bd7cd4faa99f92c88dc6ca85ae334d8
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-03-13 08:39:32 -0700

    using @new_decorator

--- sec-12/decorators.py
+++ sec-12/decorators.py
@@ -54,7 +54,8 @@ def new_decorator(original_func):
       return wrap_func()
 
 
+@new_decorator
 def func_needs_decorator():
       print(' I want to be decorated')
 
-decorated_func = new_decorator(func_needs_decorator)
+
