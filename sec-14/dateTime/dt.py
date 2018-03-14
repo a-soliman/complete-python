a06293a159ec84d2f9521fc17f7b4f1ae9693f6c
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-03-14 08:22:02 -0700

    providing an example using date

--- sec-14/dateTime/dt.py
+++ sec-14/dateTime/dt.py
@@ -7,3 +7,14 @@ print(t.minute)
 today = datetime.date.today()
 print(today)
 
+def get_birth_year( age ):
+      try:
+            num_age = int(age.strip())
+      except:
+            return False
+      
+      current_year = datetime.date.today().year
+      return current_year - num_age
+
+
+print(get_birth_year('aaa'))
